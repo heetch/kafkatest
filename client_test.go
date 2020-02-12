30d38a75f5471c4e141a4fa4102412e0f64c046c
Roger Peppe <rogpeppe@gmail.com>
2020-02-12 14:38:20 +0000

    bring on the weird

--- client_test.go
+++ client_test.go
@@ -1,92 +1,16 @@
 package kafkatest_test
 
 import (
-	"log"
+	"net"
 	"os"
 	"testing"
-	"time"
 
-	"github.com/Shopify/sarama"
-	qt "github.com/frankban/quicktest"
-
-	"github.com/heetch/kafkatest"
+	_ "github.com/heetch/kafkatest"
 )
 
-func TestNew(t *testing.T) {
-	sarama.Logger = log.New(os.Stderr, "[Sarama] ", log.LstdFlags)
-
-	c := qt.New(t)
-	k, err := kafkatest.New()
-	c.Assert(err, qt.Equals, nil)
-
-	log.Printf("************** succeeded in making initial connection")
-
-	// Produce a message to a new topic.
-	cfg := k.Config()
-	cfg.Producer.Return.Successes = true
-	cfg.Producer.Return.Errors = true
-
-	producer, err := sarama.NewSyncProducer(k.Addrs(), cfg)
-	c.Assert(err, qt.Equals, nil, qt.Commentf("addrs: %q", k.Addrs()))
-	defer producer.Close()
-	topic := k.NewTopic()
-
-	// Check that the topic has actually been created.
-	admin, err := sarama.NewClusterAdmin(k.Addrs(), cfg)
-	c.Assert(err, qt.Equals, nil)
-	defer admin.Close()
-	topics, err := admin.ListTopics()
-	c.Assert(err, qt.Equals, nil)
-	_, ok := topics[topic]
-	c.Assert(ok, qt.Equals, true)
-
-	// Produce a message to the topic.
-	_, offset, err := producer.SendMessage(&sarama.ProducerMessage{
-		Topic: topic,
-		Key:   sarama.StringEncoder("key"),
-		Value: sarama.StringEncoder("value"),
-	})
-	c.Assert(err, qt.Equals, nil)
-	c.Assert(offset, qt.Equals, int64(0))
-
-	// Check that we can consume the message we just produced.
-	consumer, err := sarama.NewConsumer(k.Addrs(), cfg)
-	c.Assert(err, qt.Equals, nil)
-	defer consumer.Close()
-	pconsumer, err := consumer.ConsumePartition(topic, 0, sarama.OffsetOldest)
-	c.Assert(err, qt.Equals, nil)
-	defer pconsumer.Close()
-	select {
-	case m := <-pconsumer.Messages():
-		c.Check(string(m.Key), qt.Equals, "key")
-		c.Check(string(m.Value), qt.Equals, "value")
-	case <-time.After(10 * time.Second):
-		c.Fatal("timed out waiting for message")
+func TestFoo(t *testing.T) {
+	_, err := net.Dial("tcp", os.Getenv("KAFKA_ADDRS"))
+	if err != nil {
+		t.Errorf("cannot dial: %v", err)
 	}
-
-	// Close the Kafka instance and check that the
-	// new topic has been removed.
-	err = k.Close()
-	c.Assert(err, qt.Equals, nil)
-	topics, err = admin.ListTopics()
-	c.Assert(err, qt.Equals, nil)
-	_, ok = topics[topic]
-	c.Assert(ok, qt.Equals, false)
-
-	// Check we can call Close again.
-	err = k.Close()
-	c.Assert(err, qt.Equals, nil)
-}
-
-func TestDisabled(t *testing.T) {
-	c := qt.New(t)
-	c.Setenv("KAFKA_DISABLE", "1")
-	k, err := kafkatest.New()
-	c.Assert(err, qt.Equals, kafkatest.ErrDisabled)
-	c.Assert(k, qt.IsNil)
-
-	c.Setenv("KAFKA_DISABLE", "bad")
-	k, err = kafkatest.New()
-	c.Assert(err, qt.ErrorMatches, `bad value for \$KAFKA_DISABLE: invalid boolean value "bad" \(possible values are: 1, t, T, TRUE, true, True, 0, f, F, FALSE\)`)
-	c.Assert(k, qt.IsNil)
 }
--- go.mod
+++ go.mod
@@ -4,7 +4,5 @@ go 1.14
 
 require (
 	github.com/Shopify/sarama v1.26.1
-	github.com/frankban/quicktest v1.7.2
-	github.com/klauspost/cpuid v1.2.1 // indirect
 	gopkg.in/retry.v1 v1.0.3
 )
